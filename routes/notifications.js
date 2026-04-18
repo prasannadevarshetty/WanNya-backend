bafb531e172baa617455465e3f6660c81484100f
Prasanna Durga <prasannadevarshetty@gmail.com>
2026-04-18 10:59:34 +0530

    Fix notifications route auth middleware and server listen for Render

--- routes/notifications.js
+++ routes/notifications.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { getNotifications } = require('../controllers/notificationController');
 const { authenticate } = require('../middleware/auth');
 
+
 router.get('/', authenticate, getNotifications);
 
 module.exports = router;
