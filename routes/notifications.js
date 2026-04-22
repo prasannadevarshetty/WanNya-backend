ce8ff18985090b576cc973b9c11e18e48b53bb45
Prasanna Durga <prasannadevarshetty@gmail.com>
2026-04-22 10:47:56 +0530

    Update Product import and schema for price handling

--- routes/notifications.js
+++ routes/notifications.js
@@ -4,6 +4,7 @@ const { getNotifications } = require('../controllers/notificationController');
 const { authenticate } = require('../middleware/auth');
 
 
+
 router.get('/', authenticate, getNotifications);
 
 module.exports = router;
