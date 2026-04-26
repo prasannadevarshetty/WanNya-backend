19dae3ba032ecdcd7278bb62988f508590b0eaff
wannya2k25 <wannyaapi96@gmail.com>
2026-04-26 19:52:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(helmet({
 // ======================
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://wannya-f.netlify.app"
+  "https://wannya-fe.netlify.app"
 ];
 
 app.use(cors({
