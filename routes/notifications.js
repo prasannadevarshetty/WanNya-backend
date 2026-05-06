fe739c8d3063ece6e7b1c1b4c5f63d5d68fcdd92
Prasanna Durga <prasannadevarshetty@gmail.com>
2026-05-06 20:54:07 +0530

    Fix OTP SMTP timeout using port 587

--- routes/notifications.js
+++ routes/notifications.js
@@ -3,11 +3,6 @@ const router = express.Router();
 const { getNotifications } = require('../controllers/notificationController');
 const { authenticate } = require('../middleware/auth');
 
-
-
-
-
-
 router.get('/', authenticate, getNotifications);
 
 module.exports = router;
