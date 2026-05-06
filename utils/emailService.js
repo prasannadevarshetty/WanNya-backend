7c225297e3d5810d5fb5cff2fba247cae440b081
Prasanna Durga <prasannadevarshetty@gmail.com>
2026-05-06 21:02:58 +0530

    Fix OTP email SMTP connection

--- utils/emailService.js
+++ utils/emailService.js
@@ -4,7 +4,8 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  
+  requireTLS: true,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -14,7 +15,7 @@ const transporter = nodemailer.createTransport({
 const sendOtpEmail = async (email, otp) => {
   try {
     const mailOptions = {
-      from: `"${process.env.FROM_NAME}" <${process.env.FROM_EMAIL}>`,
+      from: `"WanNya" <${process.env.EMAIL_USER}>`,
       to: email,
       subject: "Your WanNya OTP",
       html: `
