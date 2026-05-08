a72c218cd8b7a1f39c2532a512792d914d6e7865
wannya2k25 <wannyaapi96@gmail.com>
2026-05-08 10:19:00 +0530

    Update Cart.js

--- models/Cart.js
+++ models/Cart.js
@@ -37,12 +37,11 @@ const cartItemSchema = new mongoose.Schema({
 });
 
 const cartSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-    unique: true
-  },
+ userId: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: 'User',
+  required: true
+},
 
   items: [cartItemSchema],
 
