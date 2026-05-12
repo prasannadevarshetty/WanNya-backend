8ac9cec92cee79add4020eccf0da2161a50b629b
wannya2k25 <wannyaapi96@gmail.com>
2026-05-12 19:08:05 +0530

    Update notifications.js

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { 
-  getNotifications, 
-  deleteNotification, 
-  deleteAllNotifications, 
-  markAsRead, 
-  markAllAsRead 
+const {
+  getNotifications,
+  deleteNotification,
+  deleteAllNotifications,
+  markAsRead,
+  markAllAsRead
 } = require('../controllers/notificationController');
 
 const { authenticate } = require('../middleware/auth');
@@ -14,11 +14,11 @@ const { authenticate } = require('../middleware/auth');
 // GET all notifications
 router.get('/', authenticate, getNotifications);
 
-// MARK notification as read
-router.put('/:id/read', authenticate, markAsRead);
-
 // MARK all notifications as read
 router.put('/read-all', authenticate, markAllAsRead);
+
+// MARK single notification as read
+router.put('/:id/read', authenticate, markAsRead);
 
 // DELETE single notification
 router.delete('/:id', authenticate, deleteNotification);
