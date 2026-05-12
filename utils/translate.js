8e210b7371dcbe0a05d985d716b3f7e936d1f0fe
Lalitha Shivapriya <shivapriya032005@gmail.com>
2026-05-12 16:43:55 +0530

    Fix translation placeholder replacement

--- utils/translate.js
+++ utils/translate.js
@@ -16,7 +16,8 @@ const translate = (key, data, translations) => {
   let message = getValueFromPath(translations, key) || key;
 
   Object.keys(data || {}).forEach((k) => {
-    message = message.replace(`{{${k}}}`, data[k]);
+    const regex = new RegExp(`{{${k}}}`, 'g');
+    message = message.replace(regex, data[k]);
   });
 
   return message;
