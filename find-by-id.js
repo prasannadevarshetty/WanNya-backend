fe82d80121037d91172acc35c651fa07ebb2d8c2
Prasanna Durga <prasannadevarshetty@gmail.com>
2026-06-20 16:07:06 +0530

    remove Booking import utility and check scripts

--- find-by-id.js
+++ find-by-id.js
@@ -4,7 +4,6 @@ const Service = require('./models/Service');
 const ServiceProvider = require('./models/ServiceProvider');
 const Location = require('./models/Location');
 const Pet = require('./models/Pet');
-const Booking = require('./models/Booking');
 const Order = require('./models/Order');
 
 async function find() {
@@ -13,14 +12,6 @@ async function find() {
     console.log('MongoDB connected');
 
     const idToFind = '6a1ffaf9e426edb040d2fe46';
-
-    const booking = await Booking.findById(idToFind).populate('service');
-    if (booking) {
-      console.log('Found in Bookings collection!');
-      console.log(JSON.stringify(booking, null, 2));
-    } else {
-      console.log('Not found in Bookings collection.');
-    }
 
     const order = await Order.findById(idToFind);
     if (order) {
